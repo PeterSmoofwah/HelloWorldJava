9cdc4e87b72bd0eacbc30043b81d3b52aab6e586
PeterRodriguez <retep.smoofwah@gmail.com>
2022-04-23 13:48:04 -0600

    first Comit

---
+++ src/com/example/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, IntelliJ!");
+/*
+ Things to know:
+ 1) When you run the code it makes an out folder which is production code.
+ 2)
+ Go to File -> go to Artifacts -> click the + -> JAR -> from Modules with dependencies ->
+ click on Class with Main -> go to Build - Build the artifact -> it should be in Artifacts folder ->
+ as a .jar file -> to run the .jar press Shift twice -> Type editconfig - click on editconfig RUN ->
+ press the + button -> JAR Application -> name it example HelloWorld.jar -> Path to JAR in Artifacts Folder ->
+ Same Screen go down to Launch Section -> select HelloWorld.jar artifact -> hit Ok -> hit Ok ->
+ Now you can Run the new HelloWorld.Jar config in the top right.
+     */
+    }
+}
